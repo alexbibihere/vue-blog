3733b6476d7e84781b5faa7161a5ba1dd9893d53
alexbibihere <329731984@qq.com>
2024-08-29 22:05:20 +0800

    add

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -3,7 +3,7 @@ import { defineUserConfig } from "vuepress";
 import theme from "./theme.js";
 
 export default defineUserConfig({
-  base: "/vue-blog/",
+  base: "/vuepress/",
 
   lang: "en-US",
   title: "Yan's Blog",
@@ -11,6 +11,6 @@ export default defineUserConfig({
  
   theme,
 
-  // Enable it with pwa
+  // Enable it with pwawere
   // shouldPrefetch: false,
 });
